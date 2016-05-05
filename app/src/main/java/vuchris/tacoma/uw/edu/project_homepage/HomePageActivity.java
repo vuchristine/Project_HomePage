8b5a45096ebba82585a8fab213181759061eb93e
Christine <vuchris@uw.edu>
2016-05-05 12:47:10 -0700

    Initial commit Currently should work

---
+++ app/src/main/java/vuchris/tacoma/uw/edu/project_homepage/HomePageActivity.java
@@ -0,0 +1,79 @@
+package vuchris.tacoma.uw.edu.project_homepage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class HomePageActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //buttons
+        Button instructionsButton = (Button)findViewById(R.id.instructions_button);
+        Button startButton = (Button)findViewById(R.id.start_game);
+
+        instructionsButton.setOnClickListener(this);
+        startButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.instructions_button:
+                //Toast.makeText(this, "instructions", Toast.LENGTH_SHORT).show();
+                Intent instructionsIntent = new Intent(this, InstructionsActivity.class);
+                startActivity(instructionsIntent);
+                break;
+
+            case R.id.start_game:
+                //Toast.makeText(this, "start", Toast.LENGTH_SHORT).show();
+                Intent startIntent = new Intent(this, DifficultyLevelsActivity.class);
+                startActivity(startIntent);
+                break;
+        }
+    }
+}
